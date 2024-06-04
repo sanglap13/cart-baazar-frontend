154df7228d273add8937a293efd22a88f18284ad
sanglap13 <sanglapmridha@gmail.com>
2024-06-04 21:05:39 +0530

    feat: add initial pages and lazyLoaded them

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+//components
+import { Loader } from "./components/shared";
+
+//lazy components
+const Home = lazy(() => import("./components/pages/home/Home"));
+const Search = lazy(() => import("./components/pages/search/Search"));
+const Cart = lazy(() => import("./components/pages/cart/Cart"));
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      {/* header */}
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Home } from "./home/Home";
+export { default as Search } from "./search/Search";
+export { default as Cart } from "./cart/Cart";
