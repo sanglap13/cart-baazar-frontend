d0e8f1926f021dc78eae13bef0b615be5a5c19f2
sanglap13 <sanglapmridha@gmail.com>
2024-06-04 21:56:50 +0530

    feat: add admin panel

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,33 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 //components
 import { Loader } from "./components/shared";
 
+//admin components
+const Dashboard = lazy(() => import("./components/pages/admin/dashboard"));
+const Products = lazy(() => import("./components/pages/admin/products"));
+const Customers = lazy(() => import("./components/pages/admin/customers"));
+const Transaction = lazy(() => import("./components/pages/admin/transaction"));
+const Barcharts = lazy(
+  () => import("./components/pages/admin/charts/barcharts")
+);
+const Piecharts = lazy(
+  () => import("./components/pages/admin/charts/piecharts")
+);
+const Linecharts = lazy(
+  () => import("./components/pages/admin/charts/linecharts")
+);
+const Coupon = lazy(() => import("./components/pages/admin/apps/coupon"));
+const Stopwatch = lazy(() => import("./components/pages/admin/apps/stopwatch"));
+const Toss = lazy(() => import("./components/pages/admin/apps/toss"));
+const NewProduct = lazy(
+  () => import("./components/pages/admin/management/newproduct")
+);
+const ProductManagement = lazy(
+  () => import("./components/pages/admin/management/productmanagement")
+);
+const TransactionManagement = lazy(
+  () => import("./components/pages/admin/management/transactionmanagement")
+);
+
 //lazy components
 const Home = lazy(() => import("./components/pages/home/Home"));
 const Search = lazy(() => import("./components/pages/search/Search"));
@@ -18,7 +45,37 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/" element={<Home />} />
+
+          {/* admin routes*/}
+
+          <Route
+          // element={
+          //   <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
+          // }
+          >
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/product" element={<Products />} />
+            <Route path="/admin/customer" element={<Customers />} />
+            <Route path="/admin/transaction" element={<Transaction />} />
+            {/* Charts */}
+            <Route path="/admin/chart/bar" element={<Barcharts />} />
+            <Route path="/admin/chart/pie" element={<Piecharts />} />
+            <Route path="/admin/chart/line" element={<Linecharts />} />
+            {/* Apps */}
+            <Route path="/admin/app/coupon" element={<Coupon />} />
+            <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+            <Route path="/admin/app/toss" element={<Toss />} />
+
+            {/* Management */}
+            <Route path="/admin/product/new" element={<NewProduct />} />
+
+            <Route path="/admin/product/:id" element={<ProductManagement />} />
+
+            <Route
+              path="/admin/transaction/:id"
+              element={<TransactionManagement />}
+            />
+          </Route>
         </Routes>
       </Suspense>
     </Router>
