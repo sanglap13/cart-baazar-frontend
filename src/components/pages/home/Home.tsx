af1399cf5c0f3ab964d87bbcb8ca776f59cd12d0
sanglap13 <sanglapmridha@gmail.com>
2024-06-05 21:19:06 +0530

    feat: add basic layout of home with banner

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <section></section>
+
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
+
+      <main>
+        {/* {isLoading ? (
+          <Skeleton width="80vw" />
+        ) : (
+          data?.products.map((i) => (
+            <ProductCard
+              key={i._id}
+              productId={i._id}
+              name={i.name}
+              price={i.price}
+              stock={i.stock}
+              handler={addToCartHandler}
+              photo={i.photo}
+            />
+          ))
+        )} */}
+      </main>
+    </div>
+  );
 };
 
 export default Home;
