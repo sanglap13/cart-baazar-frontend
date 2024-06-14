a4cdba9ce00e3ec97943f6714c30b6bcdb7b94d1
sanglap13 <sanglapmridha@gmail.com>
2024-06-14 21:50:19 +0530

    feat: add search page ui

--- src/components/pages/search/Search.tsx
+++ src/components/pages/search/Search.tsx
@@ -1,7 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
+import { ProductCard } from "../../shared";
 
 const Search = () => {
-  return <div>Search</div>;
+  const [search, setSearch] = useState<string>("");
+  const [sort, setSort] = useState<string>("");
+  const [maxPrice, setMaxPrice] = useState<number>(100000);
+  const [category, setCategory] = useState<string>("");
+  const [page, setPage] = useState<number>(1);
+
+  const isPrevPage: boolean = page > 1;
+  const isNextPage: boolean = page < 4;
+
+  return (
+    <div className="product-search-page">
+      <aside>
+        <h2>Filters</h2>
+        <div>
+          <h4>Sort</h4>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="">None</option>
+            <option value="asc">Price (Low to High)</option>
+            <option value="dsc">Price (High to Low)</option>
+          </select>
+        </div>
+
+        <div>
+          <h4>Max Price: {maxPrice || ""}</h4>
+          <input
+            type="range"
+            min={100}
+            max={100000}
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(Number(e.target.value))}
+          />
+        </div>
+
+        <div>
+          <h4>Category</h4>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">ALL</option>
+            {/* {!loadingCategories &&
+              categoriesResponse?.categories.map((i) => (
+                <option key={i} value={i}>
+                  {i.toUpperCase()}
+                </option>
+              ))} */}
+          </select>
+        </div>
+      </aside>
+      <main>
+        <h1>Products</h1>
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className="search-product-list">
+          <ProductCard
+            productId="123"
+            name="name"
+            price={123}
+            stock={123}
+            handler={() => console.log("clicked")}
+            photo="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mba13-midnight-select-202402?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1708367688034"
+          />
+        </div>
+
+        {/* {productLoading ? (
+          <Skeleton length={10} />
+        ) : (
+          <div className="search-product-list">
+            {searchedData?.products.map((i) => (
+              <ProductCard
+                key={i._id}
+                productId={i._id}
+                name={i.name}
+                price={i.price}
+                stock={i.stock}
+                handler={addToCartHandler}
+                photo={i.photo}
+              />
+            ))} 
+          </div>
+        )} */}
+
+        {/* {searchedData && searchedData.totalPage > 1 && (
+          <article>
+            <button
+              disabled={!isPrevPage}
+              onClick={() => setPage((prev) => prev - 1)}
+            >
+              Prev
+            </button>
+            <span>
+              {page} of {searchedData.totalPage}
+            </span>
+            <button
+              disabled={!isNextPage}
+              onClick={() => setPage((prev) => prev + 1)}
+            >
+              Next
+            </button>
+          </article>
+        )} */}
+        <article>
+          <button
+            disabled={!isPrevPage}
+            onClick={() => setPage((prev) => prev - 1)}
+          >
+            Prev
+          </button>
+          <span>
+            {page} of {4}
+          </span>
+          <button
+            disabled={!isNextPage}
+            onClick={() => setPage((prev) => prev + 1)}
+          >
+            Next
+          </button>
+        </article>
+      </main>
+    </div>
+  );
 };
 
 export default Search;
