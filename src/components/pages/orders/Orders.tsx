dc9f1559ea9c67172d151553bd5ba9b848fa04ad
sanglap13 <sanglapmridha@gmail.com>
2024-07-24 22:01:28 +0530

    feat: add transaction management for admin

--- src/components/pages/orders/Orders.tsx
+++ src/components/pages/orders/Orders.tsx
@@ -1,22 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { TCustomError } from "../../../@types/api/api.types";
 import { TableDataType } from "../../../@types/interfaces/order.types";
 import { columnHeadings } from "../../../constants/tableHeadings";
+import { useMyOrdersQuery } from "../../../redux/api/orderApi";
+import { RootState } from "../../../redux/store";
 import TableHOC from "../../shared/admin/TableHOC";
+import { Skeleton } from "../../shared/loader/Loader";
 
 const Orders = () => {
-  const [rows, setRows] = useState<TableDataType[]>([
-    {
-      _id: "asdas",
-      amount: 3000,
+  const { user } = useSelector((state: RootState) => state.userReducer);
 
-      quantity: 2,
-      discount: 150,
+  const { isLoading, data, isError, error } = useMyOrdersQuery(user?._id!);
 
-      status: <span className="red">process</span>,
-      action: <Link to={`/orders/asdas`}>view</Link>,
-    },
-  ]);
+  const [rows, setRows] = useState<TableDataType[]>([]);
+
+  if (isError) {
+    const err = error as TCustomError;
+    toast.error(err.data.message);
+  }
 
   const Table = TableHOC<TableDataType>(
     columnHeadings,
@@ -25,11 +29,37 @@ const Orders = () => {
     "Orders",
     rows.length > 6
   )();
+
+  useEffect(() => {
+    if (data)
+      setRows(
+        data.orders.map((i) => ({
+          _id: i._id,
+          amount: i.total,
+          discount: i.discount,
+          quantity: i.orderItems.length,
+          status: (
+            <span
+              className={
+                i.status === "Processing"
+                  ? "red"
+                  : i.status === "Shipped"
+                  ? "green"
+                  : "purple"
+              }
+            >
+              {i.status}
+            </span>
+          ),
+          action: <Link to={`/admin/transaction/${i._id}`}>Manage</Link>,
+        }))
+      );
+  }, [data]);
+
   return (
     <div className="container">
       <h1>My Orders</h1>
-      {/* {isLoading ? <Skeleton length={20} /> : Table} */}
-      {Table}
+      {isLoading ? <Skeleton length={20} /> : Table}
     </div>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,6 +3,7 @@ import { productAPI } from "./api/productApi";
 import { userAPI } from "./api/userApi";
 import { userReducer } from "./reducer/userReducer";
 import { cartReducer } from "./reducer/cartReducer";
+import { orderApi } from "./api/orderApi";
 
 export const server = import.meta.env.VITE_SERVER;
 
@@ -10,13 +11,18 @@ export const store = configureStore({
   reducer: {
     [userAPI.reducerPath]: userAPI.reducer,
     [productAPI.reducerPath]: productAPI.reducer,
-    //     // [orderApi.reducerPath]: orderApi.reducer,
+    [orderApi.reducerPath]: orderApi.reducer,
     //     // [dashboardApi.reducerPath]: dashboardApi.reducer,
     [userReducer.name]: userReducer.reducer,
     [cartReducer.name]: cartReducer.reducer,
   },
 
-  middleware: (mid) => [...mid(), userAPI.middleware, productAPI.middleware],
+  middleware: (mid) => [
+    ...mid(),
+    userAPI.middleware,
+    productAPI.middleware,
+    orderApi.middleware,
+  ],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
