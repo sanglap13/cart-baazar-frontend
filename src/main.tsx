7c5c7f3b5b3bef065f15655e23d07119e4fef38c
sanglap13 <sanglapmridha@gmail.com>
2024-07-25 18:14:11 +0530

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from "./App.tsx";
 import "./styles/app.scss";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
+    <Analytics />
   </React.StrictMode>
 );
